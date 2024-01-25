63fcbea9ffaf2c634052b3f4ec1e43d2b084042f
JoaoGMagri <jgmagri@hotmail.com>
2024-01-25 17:26:53 -0300

    feat: tweets routes

--- src/main/java/com/tweteroo/api/models/TweetModel.java
+++ src/main/java/com/tweteroo/api/models/TweetModel.java
@@ -1,21 +1,20 @@
 package com.tweteroo.api.models;
 
 import java.time.Instant;
-import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.tweteroo.api.dtos.UserDTO;
+import com.tweteroo.api.dtos.TweetDTO;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,27 +24,24 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "users")
-public class UserModel {
-
+@Table(name = "tweets")
+public class TweetModel {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @Column(nullable = false)
-    private String avatar;
-    
-    @Column(nullable = false, unique = true)
-    private String username;
+    @Column(length = 200, nullable = false)
+    private String text;
 
-    @JsonIgnoreProperties("user")
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
-    private List<TweetModel> tweets;
+    @JsonIgnoreProperties("tweets")
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserModel user;
 
-
-    public UserModel(UserDTO dto){
-        this.avatar = dto.getAvatar();
-        this.username = dto.getUsername();
+    public TweetModel(TweetDTO dto, UserModel user) {
+        this.text = dto.getText();
+        this.user = user;
     }
 
 }
